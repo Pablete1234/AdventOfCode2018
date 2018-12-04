cf0b4f7a27ac6f0020701e7722cbdd9e4f9f08b1
Pablete1234 <pabloherrerapalacio@gmail.com>
2018-12-05 00:36:23 +0100

    Rework day 2 to be more stream oriented

--- src/me/pablo/advent2018/day2/Main.java
+++ src/me/pablo/advent2018/day2/Main.java
@@ -1,31 +1,30 @@
 package me.pablo.advent2018.day2;
 
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Main {
 
     // Solution for part 1
-    public static void main1(String[] args) throws FileNotFoundException {
-        int two = 0, three = 0;
-
-        Scanner sc = new Scanner(new File("input.txt"));
-
-        int minChar = 'a', size = 'z' - minChar + 1;
-
-        while (sc.hasNextLine()) {
-            int[] letterAmounts = new int[size];
-            String line = sc.nextLine();
-            for (char letter : line.toCharArray()) letterAmounts[letter-minChar]++;
-            if (Arrays.stream(letterAmounts).anyMatch(i -> i == 2)) two++;
-            if (Arrays.stream(letterAmounts).anyMatch(i -> i == 3)) three++;
-        }
-        System.out.println(two * three);
+    public static void main1(String[] args) throws IOException {
+        System.out.println(Files.lines(Paths.get("input.txt"))
+                .map(line -> Arrays.stream(line.split(""))
+                        .collect(Collectors.groupingBy(Function.identity(), Collectors.reducing(0, e -> 1, Integer::sum)))
+                        .values().stream()
+                        .filter(amount -> amount == 2 || amount == 3)
+                        .reduce(new int[2], (freq, num) -> {
+                            freq[num - 2] = 1;
+                            return freq;
+                        }, (a, b) -> { throw new UnsupportedOperationException(); })
+                ).reduce((freq1, freq2) -> {
+                    IntStream.range(0, 2).forEach(i -> freq1[i] += freq2[i]);
+                    return freq1;
+                }).map(freq -> freq[0] * freq[1]).orElse(0));
     }
 
     // Solution for part 2
@@ -41,23 +40,17 @@ public class Main {
 
     private static boolean testHash(char[][] hashes, int hashIdx) {
         char[] hash = hashes[hashIdx];
-        for (int i = 0; i < hashes.length; i++) {
-            if (hashIdx == i) continue;
-            if (fuzzyEquals(hash, hashes[i])) return true;
-        }
-        return false;
-    }
-
-    private static boolean fuzzyEquals(char[] str1, char[] str2) {
-        short diffChar = -1;
-        for (short i = 0; i < str1.length; i++) {
-            if (str1[i] == str2[i]) continue;
-            if (diffChar > -1) return false;
-            diffChar = i;
-        }
-        System.out.print(Arrays.copyOfRange(str1, 0, diffChar));
-        System.out.println(Arrays.copyOfRange(str1, diffChar + 1, str1.length));
-        return true;
+        return IntStream.range(0, hashes.length)
+                .filter(i -> i != hashIdx)
+                .anyMatch(i -> {
+                    int diffChar = IntStream.range(0, hash.length)
+                            .filter(j -> hash[j] != hashes[i][j])
+                            .limit(2) // Need max 2 matches to know it's not the correct hash
+                            .reduce(-1, (i1, i2) -> i1 == -1 ? i2 : -2);// -1 for no diff, -2 for 2+ diff. idx otherwise
+                    if (diffChar >= 0) IntStream.range(0, hash.length).filter(j -> j != diffChar)
+                            .mapToObj(j -> hash[j]).forEach(System.out::print);
+                    return diffChar >= 0;
+                });
     }
 
 }
