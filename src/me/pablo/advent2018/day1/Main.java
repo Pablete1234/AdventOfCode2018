7c7e53e0689a5bfeb019572713f1175c9b26aa92
Pablete1234 <pabloherrerapalacio@gmail.com>
2018-12-04 22:35:46 +0100

    Rework day 1 to be more stream & object oriented

--- src/me/pablo/advent2018/day1/Main.java
+++ src/me/pablo/advent2018/day1/Main.java
@@ -1,33 +1,52 @@
 package me.pablo.advent2018.day1;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.HashSet;
-import java.util.Scanner;
+import java.util.Iterator;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Main {
 
     // Solution for part 1
-    public static void main1(String[] args) throws FileNotFoundException {
-        Scanner sc = new Scanner(new File("input.txt"));
-        int total = 0;
-        while (sc.hasNextInt()) total += sc.nextInt();
-        System.out.println(total);
+    public static void main1(String[] args) throws IOException {
+        System.out.println(Files.lines(Paths.get("input.txt"))
+                .mapToInt(Integer::parseInt).sum());
     }
 
     // Solution for part 2
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException {
+        Iterator<Integer> it = new InfiniteIterator<>(Files.lines(Paths.get("input.txt"))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList()));
+
         int total = 0;
         Set<Integer> frequencies = new HashSet<>();
-
-        Scanner sc = new Scanner(new File("input.txt"));
-
-        while (frequencies.add(total)) {
-            if (!sc.hasNextInt()) sc = new Scanner(new File("input.txt"));
-            total += sc.nextInt();
-        }
+        while (frequencies.add(total)) total += it.next();
         System.out.println(total);
+    }
+
+    private static class InfiniteIterator<T> implements Iterator<T> {
+        private Iterable<T> iterable;
+        private Iterator<T> current;
+
+        InfiniteIterator(Iterable<T> iterable) {
+            this.iterable = iterable;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return true;
+        }
+
+        @Override
+        public T next() {
+            if (current == null || !current.hasNext())
+                current = iterable.iterator();
+            return current.next();
+        }
     }
 
 }
